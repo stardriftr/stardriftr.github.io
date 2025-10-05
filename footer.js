7e32b4fe31df5a5617cf3b7c936f9b14e6271609
stardriftr <calembeaz@gmail.com>
2025-10-05 21:19:41 +1300

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,3 +1,12 @@
+export function goToProfile() {
+  const username = localStorage.getItem('username');
+  if (username) {
+    window.location.href = `/user.html?username=${username}`;
+  } else {
+    alert('You must be logged in to view your profile');
+  }
+}
+
 export function createFooter() {
   const nav = document.createElement('nav');
   nav.id = 'bottom-bar';
@@ -15,19 +24,19 @@ export function createFooter() {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Notifications" tabindex="0" onclick="alert('Notifications not implemented yet')">
       <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-2 1H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6z"/>
     </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Profile" tabindex="0" onclick="goToProfile()">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Profile" tabindex="0" class="profile-icon" onclick="goToProfile()">
       <path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z"/>
     </svg>
   `;
+
+  // Dynamically attach event listener to the Profile icon
+  const profileIcon = nav.querySelector('.profile-icon');
+  if (profileIcon) {
+    profileIcon.onclick = goToProfile;
+  }
+
   return nav;
 }
 
-// Function to handle profile navigation
-function goToProfile() {
-  const username = localStorage.getItem('username');
-  if (username) {
-    window.location.href = `/user.html?username=${username}`;
-  } else {
-    alert('You must be logged in to view your profile');
-  }
-}
+// Make goToProfile globally accessible as a fallback
+window.goToProfile = goToProfile;
