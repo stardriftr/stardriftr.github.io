d55589ed12271e051f6669ef9d299e075b6bfd97
stardriftr <calembeaz@gmail.com>
2025-10-05 21:21:53 +1300

    Update footer.js

--- footer.js
+++ footer.js
@@ -12,31 +12,45 @@ export function createFooter() {
   nav.id = 'bottom-bar';
   nav.setAttribute('aria-label', 'Bottom navigation');
   nav.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Home" tabindex="0" onclick="window.location.href='index.html'">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Home" tabindex="0" class="nav-icon" data-action="goHome">
       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
     </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Search" tabindex="0" onclick="alert('Search not implemented yet')">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Search" tabindex="0" class="nav-icon" data-action="goSearch">
       <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16a6.471 6.471 0 004.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zM9.5 14A4.5 4.5 0 119.5 5a4.5 4.5 0 010 9z"/>
     </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Post" tabindex="0" onclick="window.location.href='post.html'">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Post" tabindex="0" class="nav-icon" data-action="goPost">
       <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/>
     </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Notifications" tabindex="0" onclick="alert('Notifications not implemented yet')">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Notifications" tabindex="0" class="nav-icon" data-action="goNotifications">
       <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-2 1H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6z"/>
     </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Profile" tabindex="0" class="profile-icon" onclick="goToProfile()">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-label="Profile" tabindex="0" class="nav-icon" data-action="goProfile">
       <path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z"/>
     </svg>
   `;
 
-  // Dynamically attach event listener to the Profile icon
-  const profileIcon = nav.querySelector('.profile-icon');
-  if (profileIcon) {
-    profileIcon.onclick = goToProfile;
-  }
+  // Dynamically attach event listeners to all nav icons
+  const navIcons = nav.querySelectorAll('.nav-icon');
+  navIcons.forEach(icon => {
+    const action = icon.getAttribute('data-action');
+    switch (action) {
+      case 'goHome':
+        icon.onclick = () => window.location.href = 'index.html';
+        break;
+      case 'goSearch':
+        icon.onclick = () => alert('Search not implemented yet');
+        break;
+      case 'goPost':
+        icon.onclick = () => window.location.href = 'post.html';
+        break;
+      case 'goNotifications':
+        icon.onclick = () => alert('Notifications not implemented yet');
+        break;
+      case 'goProfile':
+        icon.onclick = goToProfile;
+        break;
+    }
+  });
 
   return nav;
 }
-
-// Make goToProfile globally accessible as a fallback
-window.goToProfile = goToProfile;
